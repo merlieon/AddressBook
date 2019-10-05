7f0f12958c0f4f9c19ee2cdcd121ae87dbeeac33
merlieon <marcus.wilen@hotmail.com>
2019-10-05 11:03:55 +0200

    Adding functionallity

--- AddressBook-Server/src/Handler/ServerSocketHandler.java
+++ AddressBook-Server/src/Handler/ServerSocketHandler.java
@@ -3,6 +3,9 @@ package Handler;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ServerSocketHandler {
 
@@ -11,8 +14,8 @@ public class ServerSocketHandler {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
-    private DataOutputStream steamOut;
     private Boolean isRunning = true;
+    private List<String> addressList = new ArrayList<String>();
 
     // Adding new server socket
     public void addNewServerSocket(int port) {
@@ -24,7 +27,6 @@ public class ServerSocketHandler {
                 clientSocket = serverSocket.accept();
                 out = new PrintWriter(clientSocket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                steamOut = new DataOutputStream(clientSocket.getOutputStream());
                 ProgramLoop();
             } catch (IOException e) {
                 System.out.println("This port is already in use");
@@ -33,7 +35,11 @@ public class ServerSocketHandler {
 
     // The main loop of the program
     private void ProgramLoop() throws IOException {
-
+        // Adding items to address list
+        addressList.add("Marcus Wilen, Smallroad 14, 14390");
+        addressList.add("Martin Bolvin, Hellroad 9, 66666");
+        addressList.add("Viktor Olsson, Endroad 90, 0");
+        Boolean isFound = false;
         // Adding while loop to be able to run this multiple times
         while (isRunning) {
             WelcomeText();
@@ -41,15 +47,29 @@ public class ServerSocketHandler {
             String choice = in.readLine();
             System.out.println(choice);
             switch (choice){
+                // Printing all item to client
                 case "1":
-                    out.println("Address List");
-                    out.println("Marcus Wilen, Smallroad 14, 14390");
-                    out.println("Martin Bolvin, Hellroad 9, 66666");
-                    out.println("Viktor Olsson, Endroad 90, 0");
+                    for (String list : addressList) {
+                        out.println("");
+                        out.println(list + "\n");
+                    }
                 break;
+                // Printing search item to client
                 case "2":
-                    out.println("case 2");
+                    String search = in.readLine();
+                    for (int i = 0; i < addressList.size(); i++) {
+                        if (addressList.get(i).contains(search)){
+                            out.println("");
+                            out.println(addressList.get(i) + "\n");
+                            break;
+                        } else if (addressList.size() == i + 1){
+                            out.println("");
+                            out.println("last item \n");
+                        }
+                    }
+                    
                     break;
+                // Closing client
                 case "3":
                     serverSocket.close();
                     clientSocket.close();
