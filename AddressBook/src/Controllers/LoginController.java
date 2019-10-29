d8e912f1e8b027da4d3eb6b5319ad0ddcffd247f
merlieon <marcus.wilen@hotmail.com>
2019-10-29 21:06:32 +0100

    Adding LoginView
    # Conflicts:
    #	AddressBook-Server/src/Handler/ServerSocketHandler.java

--- AddressBook/src/Controllers/LoginController.java
+++ AddressBook/src/Controllers/LoginController.java
@@ -1,4 +1,12 @@
 package Controllers;
 
+import javafx.fxml.FXML;
+
 public class LoginController {
+
+    // Sending Login request
+    @FXML
+    void Login(){
+        System.out.println("test");
+    }
 }
--- AddressBook/src/Controllers/MainController.java
+++ AddressBook/src/Controllers/MainController.java
@@ -1,11 +1,28 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
 
+    // Opening Login window
     @FXML
-    void LoginButtonPush(){
-        System.out.println("test");
+    void LoginButtonPush(ActionEvent event){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/LoginView.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
