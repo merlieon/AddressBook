f4407101bbdb64b8c5cfae69fb6134ecd188d14a
merlieon <marcus.wilen@hotmail.com>
2019-11-03 10:50:09 +0100

    Adding styling
    # Conflicts:
    #	AddressBook-Server/src/Handler/ServerSocketHandler.java

--- AddressBook/src/Controllers/HomeController.java
+++ AddressBook/src/Controllers/HomeController.java
@@ -1,16 +1,13 @@
 package Controllers;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.IOException;
 
 public class HomeController {
@@ -32,7 +29,7 @@ public class HomeController {
        Parent root = FXMLLoader.load(getClass().getResource("/Views/CustomersView.fxml"));
        Scene scene = new Scene(root);
        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-       stage.setTitle("Login");
+       stage.setTitle("Display all customers");
        stage.setScene(scene);
        stage.show();
     }
