2016fe1928b8dd86571072228e3192a813955731
Marcus Wilen <marcus.wilen@hotmail.com>
2019-11-06 11:23:39 +0100

    Adding function to homeview

--- AddressBook/src/Controllers/HomeController.java
+++ AddressBook/src/Controllers/HomeController.java
@@ -1,17 +1,35 @@
 package Controllers;
 
+import Database.DatabaseUtility;
+import com.jfoenix.controls.JFXListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class HomeController {
+public class HomeController implements Initializable {
+    Connection con = DatabaseUtility.getConnection();
 
+    @FXML
+    JFXListView<String> listLastAddedItemsFX;
+    @FXML
+    JFXListView<String> listLastDeletedItemsFX;
     // Displaying home view
     @FXML
     public void DisplayHomeView(ActionEvent event) throws IOException{
@@ -54,5 +72,54 @@ public class HomeController {
         stage.setTitle("Deleted Customer View");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    public void Logout(ActionEvent event) throws IOException, SQLException {
+        DatabaseUtility.getConnection().close();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/LoginView.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Deleted Customer View");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Listing the last added items
+    @FXML
+    public void ListLastAddedItems() throws SQLException {
+
+        ObservableList<String> listLastAddedItems = FXCollections.observableArrayList();
+        PreparedStatement statm = con.prepareStatement("SELECT * FROM customers");
+        ResultSet rs = statm.executeQuery();
+
+        while (rs.next()){
+            listLastAddedItems.add(rs.getString(3));
+        }
+        listLastAddedItemsFX.setItems(listLastAddedItems);
+    }
+
+    // Listing the last deleted items
+    @FXML
+    public void ListLastDeletedItems() throws SQLException {
+        ObservableList<String> listLastDeletedItems = FXCollections.observableArrayList();
+        PreparedStatement statm = con.prepareStatement("SELECT * FROM customers WHERE active = 0");
+        ResultSet rs = statm.executeQuery();
+
+        while (rs.next()){
+            listLastDeletedItems.add(rs.getString(3));
+        }
+        listLastDeletedItemsFX.setItems(listLastDeletedItems);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            ListLastAddedItems();
+            ListLastDeletedItems();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
