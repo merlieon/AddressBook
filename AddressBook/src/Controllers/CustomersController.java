3ede2e0ce1604d54c58a3df64d4cfe56fc5571c7
Martin <martin.bolvin@yahoo.se>
2019-11-12 20:52:19 +0100

    Displaying customers

--- AddressBook/src/Controllers/CustomersController.java
+++ AddressBook/src/Controllers/CustomersController.java
@@ -1,16 +1,44 @@
 package Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class CustomersController {
+import Database.DatabaseUtility;
+import Models.Customer;
+
+public class CustomersController implements Initializable{
+	
+    @FXML
+    TableView<Customer> CustomersTableView;
+    @FXML
+    TableColumn<Customer,String> firstnameTableColumn;
+    @FXML
+    TableColumn<Customer,String> lastnameTableColumn;
+    @FXML
+    TableColumn<Customer,String> phonenumberTableColumn;
+    @FXML
+    TableColumn<Customer,String> emailTableColumn;
+    
+	Connection con = DatabaseUtility.getConnection();
 
     // Displaying home view
     @FXML
@@ -55,4 +83,42 @@ public class CustomersController {
         stage.setScene(scene);
         stage.show();
     }
+    
+    
+    
+    
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        firstnameTableColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("firstName"));
+        lastnameTableColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("lastName"));
+        emailTableColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("email"));
+        phonenumberTableColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("phoneNumber"));
+        try {
+            CustomersTableView.getItems().setAll(listCustomers());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ObservableList<Customer> listCustomers() throws SQLException {
+        ObservableList<Customer> listCustomers = FXCollections.observableArrayList();
+        PreparedStatement statm = con.prepareStatement("SELECT * FROM customers");
+        ResultSet rs = statm.executeQuery();
+
+        while (rs.next()){
+            String firstname = rs.getString("firstname");
+            String lastname = rs.getString("lastname");
+            String email = rs.getString("email");
+            String phonenumber = rs.getString("phonenumber");
+
+            listCustomers.addAll(new Customer(firstname, lastname, email, phonenumber));
+        }
+
+        return  listCustomers;
+    }
+    
+    
+    
+    
 }
